70da29b448e2c5b0e5254da8ef96bf40a7257f37
angelalita77 <anjeanette@gmail.com>
2025-10-01 13:46:50 -0400

    Add the routes for three pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import { useState } from 'react'
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Films from './pages/Films'
+import People from './pages/People'
+import Starships from './pages/Starships'
+import Planets from './pages/Planets'
 
 function App() {
-  const [count, setCount] = useState(0)
+  //setup our routes & route components
+  // create 3 page components to user with the routes
+  //  // people, starships, planets
+  // Create a nav bar to navigate between pages
 
   return (
     <>
-     
+     <Routes>
+      <Route path='/people' element={<People />}></Route>
+      <Route path='/ships' elements={<Starships />}></Route>
+      <Route path='/planets' elements={<Planets  />}></Route>
+     </Routes>
     </>
   )
 }
